482d65b4c7cf1f7b13b15c21fc2a523f5e393d4f
Alexph7 <alexph7.56@gmail.com>
2021-10-29 17:47:47 -0300

    AulaHerança

---
+++ src/com/alex/aulaheranca/AulaHeranca.java
@@ -0,0 +1,12 @@
+package com.alex.aulaheranca;
+
+public class AulaHeranca {
+
+        public static void main(String[] args) {
+
+                ComissaoEmpregado emp = new ComissaoEmpregado("Alex", "Souza", "777777", 1000, 0.3);
+                
+                System.out.println(emp);
+        }
+        
+}
