13e90cd9865165e67de7d14c34812ff5c7e663f9
Alexph7 <alexph7.56@gmail.com>
2021-10-29 23:47:10 -0300

    finish

--- src/com/alex/aulaheranca/ComissaoEmpregado.java
+++ src/com/alex/aulaheranca/ComissaoEmpregado.java
@@ -2,13 +2,13 @@ package com.alex.aulaheranca;
 
 public class ComissaoEmpregado {
 
-        protected final String nome;
-        protected final String sobreNome;
-        protected final String numSeguroSocial;
-        protected double vendaBruta;
-        protected double taxaComissao; //porcentagem da comissão
+        private final String nome;
+        private final String sobreNome;
+        private final String numSeguroSocial;
+        private double vendaBruta;
+        private double taxaComissao; //porcentagem da comissão
 
-        public ComissaoEmpregado(String nome, String sobreNome, String numSeguroSocial, double vendaBruta, double taxaComissao) {
+        public ComissaoEmpregado(String nombre, String sobreNome, String numSeguroSocial, double vendaBruta, double taxaComissao) {
 
                 if (vendaBruta < 0) { //Se Vendas Brutas for invalido, lança uma exceção
                         throw new IllegalArgumentException("Vendas Brutas Deve Ser Maior Que 0");
@@ -18,7 +18,7 @@ public class ComissaoEmpregado {
                         throw new IllegalArgumentException("Taxa de Comissão deve ser maior que 0");
                 }
 
-                this.nome = nome;
+                this.nome = nombre;
                 this.sobreNome = sobreNome;
                 this.numSeguroSocial = numSeguroSocial;
                 this.vendaBruta = vendaBruta;
@@ -42,7 +42,7 @@ public class ComissaoEmpregado {
         }
 
         public void setVendaBruta(double vendaBruta) {
-                  if (vendaBruta < 0) { //Se Vendas Brutas for invalido, lança uma exceção
+                if (vendaBruta < 0) { //Se Vendas Brutas for invalido, lança uma exceção
                         throw new IllegalArgumentException("Vendas Brutas Deve Ser Maior Que 0");
                 }
                 this.vendaBruta = vendaBruta;
@@ -53,21 +53,22 @@ public class ComissaoEmpregado {
         }
 
         public void setTaxaComissao(double taxaComissao) {
-                 if (taxaComissao < 0 && taxaComissao > 1.0) {
+                if (taxaComissao < 0 && taxaComissao > 1.0) {
                         throw new IllegalArgumentException("Taxa de Comissão deve ser maior que 0");
                 }
                 this.taxaComissao = taxaComissao;
         }
-        
-        public double ganhoDeComissao(){
-         return vendaBruta * taxaComissao;       
+
+        public double ganhoDeComissao() {
+                return getVendaBruta() * getTaxaComissao();
         }
 
         @Override
         public String toString() { // significa que esse método substitui um método da superclasse
-                return String.format("%s: %s %s %n%s%s %n%s%.2f$ %n%s%.2f" ,"Nome Do Empregado",nome, sobreNome,
-                "Numero do Seguro Social ", numSeguroSocial, "Venda Bruta ", vendaBruta, "Taxa de Comissão ", taxaComissao);
+                return String.format("%s: %s %s %n%s%s %n%s%.2f$ %n%s%.2f",
+                            "Nome Do Empregado", getNome(), getSobreNome(),
+                            "Numero do Seguro Social ", getNumSeguroSocial(),
+                            "Venda Bruta ", getVendaBruta(),
+                            "Taxa de Comissão ", getTaxaComissao());
         }
-
-        
 }
