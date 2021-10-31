0fc2243a407678709505fb188e77e594cb3d5b53
Alexph7 <alexph7.56@gmail.com>
2021-10-30 21:12:00 -0300

    Arquivos Externos

--- src/com/alex/aulaheranca/AulaHeranca.java
+++ src/com/alex/aulaheranca/AulaHeranca.java
@@ -4,9 +4,5 @@ public class AulaHeranca {
 
         public static void main(String[] args) {
 
-                ComissaoEmpregado emp = new ComissaoEmpregado("Alex", "Souza", "777777", 1000, 0.3);
-                
-                System.out.println(emp);
         }
-        
 }
